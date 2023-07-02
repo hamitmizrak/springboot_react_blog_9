3fefa8d64f3e33266f72618055f298c7bbfde3bd
hamitmizrak <hamitmizrak@gmail.com>
2023-07-02 09:46:44 +0300

    Project all config

--- src/main/java/com/hamitmizrak/springboot_react_blog_9/SpringbootReactBlog9Application.java
+++ src/main/java/com/hamitmizrak/springboot_react_blog_9/SpringbootReactBlog9Application.java
@@ -1,12 +1,63 @@
 package com.hamitmizrak.springboot_react_blog_9;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+// @EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+// @EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository") //Repository bulamadığı zaman
+
+// Spring Cache aktif etmek gerekiyor
+// @EnableCaching
+
+// auditorAware icin
+//@EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
+
+// Spring Security
+@SpringBootApplication(exclude = {
+		//SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+/*@SpringBootApplication*/
 public class SpringbootReactBlog9Application {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
+
 	public static void main(String[] args) {
+
+		// devtools active isActive
+		//System.setProperty("spring.devtools.restart.enabled","true");
+
+		// PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+         */
+
+		//Disables headless JOptionPane
+		System.setProperty("java.awt.headless", "false");
+
 		SpringApplication.run(SpringbootReactBlog9Application.class, args);
 	}
 
