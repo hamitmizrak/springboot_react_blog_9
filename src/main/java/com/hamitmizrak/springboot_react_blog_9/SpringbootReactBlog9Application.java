bf562ba54ae37715a71ebe8e45a405ab6bd40c42
hamitmizrak <hamitmizrak@gmail.com>
2023-07-02 09:35:11 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/springboot_react_blog_9/SpringbootReactBlog9Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.springboot_react_blog_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactBlog9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactBlog9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamitmizrak/springboot_react_blog_9/SpringbootReactBlog9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.springboot_react_blog_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactBlog9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
